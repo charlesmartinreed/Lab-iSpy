2e010b01480213d01cb7c78336c223d87f7d0080
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-11-07 16:28:58 -0600

    Scrollable hedgehog!

--- Lab-iSpy/ViewController.swift
+++ Lab-iSpy/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    //MARK:- @IBOutlets
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set the view's delegate
+        scrollView.delegate = self
+        
+        updateZoomFor(size: view.bounds.size)
+        
+    }
+    
+    //MARK:- delegate method
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        //updates the zoom size of the image
+        
+        //use imageView and size parameter - screen bounds here - to calculate the scale
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale) //scaled by the smaller of these two amounts
+        
+        // set the minimumZoomScale prop of the scrollView to the derived scale
+        scrollView.minimumZoomScale = scale
     }
 
 
